1cd10624e6e5f4c56cbcf72f7ad527f93aa06743
Delia <delia.diaconita@gmail.com>
2017-01-08 23:48:42 +0200

    First commit

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,34 @@
+package hello;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+/**
+ * Created by Delia on 1/8/2017.
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(final ApplicationContext context) {
+        return args -> {
+            System.out.println("Inspect the bean");
+
+            String[] beanNames = context.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+        };
+    }
+}
